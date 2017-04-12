d40c4249e390c6745f2df23971aa51e057a7ed53
Petar Metodiev <petar.metodiev@gmail.com>
2017-04-12 23:00:11 +0300

    Add clarifying comments for GPIO numbering

--- 01_led.js
+++ 01_led.js
@@ -1,5 +1,8 @@
 const Gpio = require('onoff')
 	.Gpio;
+// onoff uses GPIO numbering instead of physical numbering
+// https://www.raspberrypi.org/documentation/usage/gpio-plus-and-raspi2/ scroll down to APPENDIX 1. A NOTE ON PIN NUMBERING
+// Numbering: http://www.raspberrypi-spy.co.uk/wp-content/uploads/2012/06/Raspberry-Pi-GPIO-Layout-Model-B-Plus-rotated-2700x900.png
 const led = new Gpio(22, 'out');
 
 const toggleState = (curr) => curr === 1 ? 0 : 1;
